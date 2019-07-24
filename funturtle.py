d2384dfbcf11763b08727bea0df19cd4854f648f
leen21-meet <leen21@meet.mit.edu>
2019-07-24 05:04:32 -0400

    Stopping here

---
+++ funturtle.py
@@ -0,0 +1,62 @@
+import turtle
+##part 2 and 3
+##turtle.shape('turtle')
+##finn=turtle.clone()
+##finn.shape('square')
+##finn.goto (100,100)
+##
+##finn.penup()
+##finn.goto (150,0)
+##finn.pendown()
+##finn.goto (200,0)
+##finn.goto (200,50)
+##finn.goto (150,50)
+##finn.goto (150,0)
+##
+##turtle.shape('triangle')
+##charlie=turtle.clone()
+##charlie.penup()
+##charlie.goto (-50,0)
+##charlie.pendown()
+##charlie.goto (-150,0)
+##charlie.goto (-150,100)
+##charlie.goto (-50,0)
+##
+##finn.goto (300,300)
+##id1=finn.stamp()
+##finn.goto (100,100)
+##id2=finn.stamp()
+##finn.goto(200,100)
+##finn.clearstamp(id1)
+##finn.clearstamp(id2)
+
+## part 4444
+##boo=turtle.Turtle ()
+##
+##turtle.bgcolor ('purple')
+##boo.color('green')
+##boo.pensize (5)
+##boo.goto (0,-100)
+##boo.goto (50,-100)
+##boo.color ('blue')
+##
+##foo=turtle.Turtle ()
+##foo.color ('yellow')
+##foo.penup()
+##foo.goto (0,0)
+##foo.pendown()
+##foo.goto (0,100)
+##foo.goto (-50,100)
+##foo.color ('white')
+##foo.goto (-50,0)
+##foo.shape('circle')
+
+##part555555555
+def up()
+turtle.forward
+
+
+
+           
+
+
